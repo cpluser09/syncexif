0aa5030683af7461ee9ea4bfe926a4fac32542ef
cpluserlin <jim.ncut@gmail.com>
2020-11-03 10:08:32 +0800

    add logs

--- sync_pictures.py
+++ sync_pictures.py
@@ -3,8 +3,8 @@ import sys
 import shutil
 
 FOLDER_SOURCE = "/Users/junlin/myPhoto"
-#FOLDER_DESTINATION = "/Users/junlin/myPhoto2"
-FOLDER_DESTINATION = "/Volumes/myPhoto"
+FOLDER_DESTINATION = "/Users/junlin/myPhoto2"
+#FOLDER_DESTINATION = "/Volumes/myPhoto"
 FILE_FILTERS = [".jpg", ".JPG", ".jpeg", ".JPEG", ".raf", ".RAF", ".png", ".PNG"]
 
 global G_TOTAL_SYNC_COUNT
@@ -15,14 +15,19 @@ def calc_dst_folder(source_file):
     return file_full_path
 
 def sync_file(source_file):
+    global G_TOTAL_SYNC_COUNT
+    print("\ntry sync:", source_file)
     dst_file = calc_dst_folder(source_file)
     if os.path.exists(dst_file) == True:
+        print("skip...")
         return False
     dst_folder,_ = os.path.splitext(dst_file)
     dst_folder = dst_folder[0: dst_folder.rfind("/")]
     if os.path.exists(dst_folder) == False:
         os.makedirs(dst_folder)
     shutil.copyfile(source_file, dst_file)
+    G_TOTAL_SYNC_COUNT = G_TOTAL_SYNC_COUNT + 1
+    print("sync  to:", dst_folder, "                ", G_TOTAL_SYNC_COUNT)
     return True
 
 def search_files(root_name, filter, result):
@@ -35,9 +40,7 @@ def search_files(root_name, filter, result):
             file_name_with_path, ext = os.path.splitext(file_path)
             file_name = file_name_with_path [file_name_with_path.rfind("/")+1: len(file_name_with_path)]
             if ext in filter and file_name[0] != "." and "Trashes" not in file_name_with_path:
-                if sync_file(file_path):
-                    G_TOTAL_SYNC_COUNT = G_TOTAL_SYNC_COUNT + 1
-                    print("sync", G_TOTAL_SYNC_COUNT, ":", file_path)
+                sync_file(file_path)
                 result.append(file_path)
     return result
 
