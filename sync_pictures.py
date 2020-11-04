cbcd08dccec4738daf28f3e404d857e9a7982a0f
cpluserlin <jim.ncut@gmail.com>
2020-11-04 16:09:04 +0800

    add custom progress bar sample code

--- sync_pictures.py
+++ sync_pictures.py
@@ -15,6 +15,37 @@ class FancyBar(Bar):
     fill = '*'
     suffix = '%(percent).1f%% - %(elapsed)ds [remaining %(remaining)d - total %(max)d]'
 
+import math
+
+# class ProcessBar(object):
+#     """一个打印进度条的类"""
+#     def __init__(self, total):  # 初始化传入总数
+#         self.shape = ['▏', '▎', '▍', '▋', '▊', '▉']
+#         self.shape_num = len(self.shape)
+#         self.row_num = 30
+#         self.now = 0
+#         self.total = total
+
+#     def print_next(self, now=-1):   # 默认+1
+#         if now == -1:
+#             self.now += 1
+#         else:
+#             self.now = now
+            
+#         rate = math.ceil((self.now / self.total) * (self.row_num * self.shape_num))
+#         head = rate // self.shape_num
+#         tail = rate % self.shape_num
+#         info = self.shape[-1] * head
+#         if tail != 0:
+#             info += self.shape[tail-1]
+#         full_info = '[%s%s] [%.2f%%]' % (info, (self.row_num-len(info)) * '  ', 100 * self.now / self.total)
+
+#         print("\r", end='', flush=True)
+#         print(full_info, end='', flush=True)
+
+#         if self.now == self.total:
+#             print('')    
+
 def calc_dst_folder(source_file):
     file_with_sub_folder = source_file.replace(FOLDER_SOURCE, "")
     file_full_path = FOLDER_DESTINATION + file_with_sub_folder
@@ -69,6 +100,9 @@ def let_go():
     result = search_files(folder, FILE_FILTERS, result)
     sync(result)
     print("DONE. total %d source files, %d files synced." % (len(result), G_TOTAL_SYNC_COUNT))
+    # pb = ProcessBar(10000)
+    # for i in range(10000):
+    #     pb.print_next()
 
 if __name__ == '__main__':
     let_go()
