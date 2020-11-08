1200e095f593129277ae99a6c85b40e1a588d53c
cpluser <enjoyimage@163.com>
2020-11-08 20:29:21 +0800

    change default folder

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from backup_picture import backup_pictures
 from sync_pictures import sync_pictures
+from sync_exif import sync_exif
 
 FOLDER_SOURCE = "/Volumes/FUJI"
-FOLDER_BACKUP = "/Users/junlin/myPhoto/Photography18/xxx"
+FOLDER_BACKUP = "/Users/junlin/myPhoto/new/Photography18/20201107_人民广场"
 
 FOLDER_SYNC_SOURCE = "/Users/junlin/myPhoto"
 FOLDER_SYNC_DESTINATION = "/Volumes/myPhoto"
@@ -11,5 +12,6 @@ FILE_FILTERS = [".jpg", ".JPG", ".jpeg", ".JPEG", ".raf", ".RAF", ".png", ".PNG"
 
 
 if __name__ == '__main__':
-    backup_pictures(FOLDER_SOURCE, FOLDER_BACKUP, FILE_FILTERS)
+    #backup_pictures(FOLDER_SOURCE, FOLDER_BACKUP, FILE_FILTERS)
+    #sync_exif(FOLDER_BACKUP)
     sync_pictures(FOLDER_SYNC_SOURCE, FOLDER_SYNC_DESTINATION, FILE_FILTERS)
